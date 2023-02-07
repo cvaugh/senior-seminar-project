a92f5e664520a0fe01d8d62ef1306e7f3c3a3db8
Chase Vaughan <git@cvaugh.dev>
2023-02-07 16:48:10 -0500

    Begin implementing UI

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,12 +4,27 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour {
     public float globalGrowthRate = 5.0f;
+    public GameObject inventoryUI;
+    public GameObject openInventoryButton;
+    private PlayerController player;
 
     void Start() {
-        
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+        HideInventory();
     }
 
     void Update() {
-        
+
+    }
+
+    public void ShowInventory() {
+        player.SortInventory();
+        openInventoryButton.SetActive(false);
+        inventoryUI.SetActive(true);
+    }
+
+    public void HideInventory() {
+        inventoryUI.SetActive(false);
+        openInventoryButton.SetActive(true);
     }
 }
