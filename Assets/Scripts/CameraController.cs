7f17f9f1ac4a7e457247288176707ce538fbba17
Jaakob Alakulppi <jbalakulppi@gmail.com>
2023-02-07 15:18:52 -0500

    Added a smooth camera controller

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour {
 
+    private Transform target;
+    private Vector3 offset;
+
+    public float cameraSpeed = 0.35f;
+    
     // Start is called before the first frame update
     void Start() {
-        
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        offset = transform.position - target.position;
     }
 
-    // Update is called once per frame
-    void Update() {
-        
+    // LateUpdate is called after all Update functions have been called
+    void LateUpdate() {
+        //transform.position = target.position + offset;
+        Vector3 desiredPosition = target.position + offset;
+        transform.position  = Vector3.Lerp(transform.position, desiredPosition, cameraSpeed);
     }
 }
