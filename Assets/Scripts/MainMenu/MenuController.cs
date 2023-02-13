0efd86a5be4abe2a9e546bd1ffd3c3053af0e0e8
Chase Vaughan <git@cvaugh.dev>
2023-02-12 22:14:22 -0500

    Remove bees from main menu

--- Assets/Scripts/MainMenu/MenuController.cs
+++ Assets/Scripts/MainMenu/MenuController.cs
@@ -6,22 +6,9 @@ using UnityEngine.SceneManagement;
 public class MenuController : MonoBehaviour {
     public Transform trellis;
     public Transform loadingBlocker;
-    public float beeRange;
-    public float beeForgetRange;
-    public float beeSpeed;
-    public float beeMaxSpeed;
-    public float beeTurnSpeed;
-    public Vector2 screenMin;
-    public Vector2 screenMax;
 
     void Start() {
         loadingBlocker.gameObject.SetActive(false);
-        RectTransform canvas = GameObject.Find("Canvas").GetComponent<RectTransform>();
-        Vector3 size = new Vector3(canvas.sizeDelta.x / 2, canvas.sizeDelta.y / 2);
-        canvas.position = Vector3.zero;
-        Debug.Log(canvas.position + " +++ " + canvas.sizeDelta + " +++ " + (canvas.position - size));
-        screenMin = Camera.main.ScreenToWorldPoint(new Vector2(0, 0));
-        screenMax = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
     }
 
     void Update() {
