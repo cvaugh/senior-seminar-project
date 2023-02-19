398aea34f2b0c49f7f811de348397512c09688c1
Chase Vaughan <git@cvaugh.dev>
2023-02-19 13:02:45 -0500

    Fix syntax and project structure

--- Assets/Scripts/LoadingManager.cs
+++ Assets/Scripts/LoadingManager.cs
@@ -4,13 +4,10 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class LoadingManager : MonoBehaviour
-{
-    public static LoadingManager Instance;
+public class LoadingManager : MonoBehaviour {
+    public static LoadingManager instance;
 
-   // public GameObject mainManager;
-
-    public GameObject LoadingPanel;
+    public GameObject loadingPanel;
     public float minLoadTime;
 
     public GameObject loadingWheel;
@@ -22,74 +19,61 @@ public class LoadingManager : MonoBehaviour
     public Image fadeImage;
     public float fadeTime;
 
-    private void Awake()
-    {
-        if (Instance == null)
-        {
-            Instance = this;
+    private void Awake() {
+        if(instance == null) {
+            instance = this;
             DontDestroyOnLoad(gameObject);
         }
-        //mainManager.SetActive(false);
-        LoadingPanel.SetActive(false);
+        loadingPanel.SetActive(false);
         fadeImage.gameObject.SetActive(false);
     }
 
-    public void LoadScene(string sceneName)
-    {
+    public void LoadScene(string sceneName) {
         targetScene = sceneName;
         StartCoroutine(LoadSceneRoutine());
     }
 
-    private IEnumerator LoadSceneRoutine()
-    {
+    private IEnumerator LoadSceneRoutine() {
         isLoading = true;
 
         fadeImage.gameObject.SetActive(true);
         fadeImage.canvasRenderer.SetAlpha(0);
 
-      while (!Fade(1))
-        {
+        while(!Fade(1)) {
             yield return null;
         }
     
-        LoadingPanel.SetActive(true);
+        loadingPanel.SetActive(true);
         StartCoroutine(SpinWheelRoutine());
 
         
-        while (!Fade(0))
-        {
+        while(!Fade(0)) {
             yield return null;
         }
-
-        
 
         AsyncOperation op = SceneManager.LoadSceneAsync(targetScene);
         float elapsedTime = 0f;
 
-        while (!op.isDone)
-        {
+        while(!op.isDone) {
             elapsedTime += Time.deltaTime;
             yield return null;
         }
 
-        while (elapsedTime < minLoadTime)
-        {
+        while(elapsedTime < minLoadTime) {
             elapsedTime += Time.deltaTime;
             yield return null;
         }
 
         
-        while (!Fade(1))
-        {
+        while(!Fade(1)) {
             yield return null;
         }
         
 
-        LoadingPanel.SetActive(false);
+        loadingPanel.SetActive(false);
 
         
-        while (!Fade(0))
-        {
+        while (!Fade(0)) {
             yield return null;
         }
         
@@ -98,11 +82,9 @@ public class LoadingManager : MonoBehaviour
     }
 
    
-    private bool Fade(float target)
-    {
+    private bool Fade(float target) {
         fadeImage.CrossFadeAlpha(target, fadeTime, true);
-        if (Mathf.Abs(fadeImage.canvasRenderer.GetAlpha() - target) <= 0.05f)
-        {
+        if(Mathf.Abs(fadeImage.canvasRenderer.GetAlpha() - target) <= 0.05f) {
             fadeImage.canvasRenderer.SetAlpha(target);
             return true;
         }
@@ -111,10 +93,8 @@ public class LoadingManager : MonoBehaviour
     }
    
 
-    private IEnumerator SpinWheelRoutine()
-    {
-        while (isLoading)
-        {
+    private IEnumerator SpinWheelRoutine() {
+        while(isLoading) {
             loadingWheel.transform.Rotate(0, 0, -wheelSpeed);
             yield return null;
         }
