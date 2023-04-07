cd269772228dd918f51e9d383a597e10a53f2cac
Jaakob Alakulppi <jbalakulppi@gmail.com>
2023-04-07 19:55:46 -0400

    7 day Weather forecast added

---
+++ Assets/Scripts/TimeOfDay.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeOfDay : MonoBehaviour
+{
+    public float dayDurationSeconds = 10f;
+    public Light sun;
+    public float currentTimeOfDay = 0f;
+    private float timeMultiplier = 1f;
+    private float sunInitialIntensity;
+    private int currentDayOfWeek = 1;
+    private int previousDayOfWeek;
+    public int weeks = 0;
+
+    private void Start() {
+        sun = GameObject.Find("Directional Light").GetComponent<Light>();
+
+        if (sun == null) {
+            Debug.LogError("No directional light found");
+        }
+        sunInitialIntensity = sun.intensity;
+    }
+
+    private void UpdateSun() {
+        sun.transform.localRotation = Quaternion.Euler((currentTimeOfDay * 360f) - 90, 170, 0);
+
+        float intensityMultiplier = 1;
+        if (currentTimeOfDay <= 0.23f || currentTimeOfDay >= 0.75f) {
+            intensityMultiplier = 0;
+        }
+        else if (currentTimeOfDay <= 0.25f) {
+            intensityMultiplier = Mathf.Clamp01((currentTimeOfDay - 0.23f) * (1 / 0.02f));
+        }
+        else if (currentTimeOfDay >= 0.73f) {
+            intensityMultiplier = Mathf.Clamp01(1 - ((currentTimeOfDay - 0.73f) * (1 / 0.02f)));
+        }
+
+        sun.intensity = sunInitialIntensity * intensityMultiplier;
+    }
+
+    private void Update() {
+        UpdateSun();
+
+        currentTimeOfDay += (Time.deltaTime / dayDurationSeconds) * timeMultiplier;
+
+        if (currentTimeOfDay >= 1) {
+            currentTimeOfDay = 0;
+            currentDayOfWeek ++;
+
+            if (currentDayOfWeek > 7) {
+                currentDayOfWeek = 1;
+                weeks ++;
+            }
+        }
+    }
+
+    public int GetCurrentDay() {
+        return currentDayOfWeek;
+    }
+
+    public int GetCurrentWeek() {
+        return weeks;
+    }
+
+    public int GetPreviousDay() {
+        int previousDayOfWeek = currentDayOfWeek - 1;
+        if (previousDayOfWeek == 0) {
+            previousDayOfWeek = 7;
+        }
+        return previousDayOfWeek;
+    }
+    //method for isDayTime(), isAfternoon(), isNightTime()
+}
--- Assets/Scripts/WeatherManager.cs
+++ Assets/Scripts/WeatherManager.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class WeatherManager : MonoBehaviour
 {
-    public enum Season {NONE, SPRING, SUMMER, AUTUMN, WINTER};
-    public enum Weather {NONE, SUNNY, HOTSUN, RAIN, SNOW};
+    public enum Season {NONE, SPRING, SUMMER, AUTUMN, WINTER}; //MONSOON, DROUGHT
+    public enum Weather {NONE, SUNNY, HOTSUN, RAIN, SNOW, HEAVYRAIN, FOGGY, WINDY, THUNDERSTORM}; //DUST
 
     public Season currentSeason;
     public Weather currentWeather;
 
     public ParticleSystem rain;
-    //public ParticleSystem heavyRain;
-    //public ParticleSystem snow;
+    public ParticleSystem heavyRain;
+    public ParticleSystem snow;
+    public ParticleSystem fog;
+    public ParticleSystem wind;
+    public ParticleSystem storm;
+    // public ParticleSystem dust;
 
     [Header ("Time settings")]
     public float seasonTime;
@@ -20,6 +24,8 @@ public class WeatherManager : MonoBehaviour
     public float summerTime;
     public float autumnTime;
     public float winterTime;
+
+    public int currentYear;
 
     [Header ("Light settings")]
     public Light sunlight;
@@ -33,7 +39,25 @@ public class WeatherManager : MonoBehaviour
     public Color autumnColor;
     public Color winterColor;
 
-    public int currentYear;
+    public TimeOfDay timeOfDay;
+    public int currentWeek = 0;
+    public List<Weather> weeklyForecast;
+    private bool forecastGenerated = false;
+
+    // [Header("UI Elements")]
+    // public Text currentSeasonText;
+    // public Text currentWeatherText;
+    // public Image currentWeatherImage;
+    // public Sprite sunnySprite;
+    // public Sprite rainSprite;
+    // public Sprite heavyRainSprite;
+    // public Sprite stormSprite;
+    // public Sprite snowSprite;
+    // public Sprite windSprite;
+    // public Sprite fogSprite;
+
+    // public Text[] forecastDaysText;
+    // public Image[] forecastDaysImage;
 
     private void Start() {
         this.currentSeason = Season.SPRING;
@@ -42,9 +66,146 @@ public class WeatherManager : MonoBehaviour
 
         this.seasonTime = this.springTime;
         this.rain.Stop();
+        this.snow.Stop();
+        // this.heavyRain.Stop();
+        // this.fog.Stop();
+        // this.wind.Stop();
+        // this.storm.Stop();
 
         this.defaultLightColor = this.sunlight.color;
         this.defaultLightIntensity = this.sunlight.intensity;
+        if (!forecastGenerated) {
+            weeklyForecast = GenerateWeeklyForecast();
+            forecastGenerated = false;
+            currentWeek = timeOfDay.GetCurrentWeek();
+        }
+    }
+
+    private void UpdateWeather() {
+        int currentDayOfWeek = timeOfDay.GetCurrentDay();
+        if (timeOfDay.GetPreviousDay() == 7 && timeOfDay.GetCurrentWeek() != currentWeek) {
+            weeklyForecast = GenerateWeeklyForecast();
+            currentWeek = timeOfDay.GetCurrentWeek();
+        }
+
+        switch (currentDayOfWeek) {
+            case 1:
+                ChangeWeather(this.weeklyForecast[currentDayOfWeek - 1]);
+                break;
+            case 2:
+                ChangeWeather(this.weeklyForecast[currentDayOfWeek - 1]);
+                break;
+            case 3:
+                ChangeWeather(this.weeklyForecast[currentDayOfWeek - 1]);
+                break;
+            case 4:
+                ChangeWeather(this.weeklyForecast[currentDayOfWeek - 1]);
+                break;
+            case 5:
+                ChangeWeather(this.weeklyForecast[currentDayOfWeek - 1]);
+                break;
+            case 6:
+                ChangeWeather(this.weeklyForecast[currentDayOfWeek - 1]);
+                break;
+            case 7:
+                ChangeWeather(this.weeklyForecast[currentDayOfWeek - 1]);
+                break;
+        }
+    }
+
+    public List<Weather> GenerateWeeklyForecast() {
+        Season curSeason = this.currentSeason;
+
+        List<Weather> weeklyWeather = new List<Weather>();
+        for (int i = 0; i < 7; i++) {
+            Weather weather = RandomizeWeather(curSeason);
+            weeklyWeather.Add(weather);
+        }
+        return weeklyWeather;
+    }
+
+    private Weather RandomizeWeather(Season season) {
+        switch (season) {
+            case Season.SPRING:
+                float springWeather = Random.Range(0f, 1f);
+                if (springWeather < 0.4f) {
+                    return Weather.RAIN;
+                }
+                else if (springWeather < 0.6f) {
+                    return Weather.SUNNY;
+                }
+                else if (springWeather < 0.7f) {
+                    return Weather.WINDY;
+                }
+                else if (springWeather < 0.8f) {
+                    return Weather.FOGGY;
+                }
+                else if (springWeather < 0.9f) {
+                    return Weather.THUNDERSTORM;
+                }
+                else {
+                    return Weather.NONE;
+                }
+                break;
+            case Season.SUMMER:
+                float summerWeather = Random.Range(0f, 1f);
+                if (summerWeather < 0.3f) {
+                    return Weather.SUNNY;
+                }
+                else if (summerWeather < 0.6f) {
+                    return Weather.HOTSUN;
+                }
+                else if (summerWeather < 0.7f) {
+                    return Weather.RAIN;
+                }
+                else if (summerWeather < 0.8f) {
+                    return Weather.WINDY;
+                }
+                else if (summerWeather < 0.9f) {
+                    return Weather.THUNDERSTORM;
+                }
+                else {
+                    return Weather.NONE;
+                }
+                break;
+            case Season.AUTUMN:
+                float autumnWeather = Random.Range(0f, 1f);
+                if (autumnWeather < 0.3f) {
+                    return Weather.SUNNY;
+                }
+                else if (autumnWeather < 0.6f) {
+                    return Weather.HOTSUN;
+                }
+                else if (autumnWeather < 0.7f) {
+                    return Weather.RAIN;
+                }
+                else if (autumnWeather < 0.8f) {
+                    return Weather.WINDY;
+                }
+                else if (autumnWeather < 0.9f) {
+                    return Weather.THUNDERSTORM;
+                }
+                else {
+                    return Weather.NONE;
+                }
+                break;
+            case Season.WINTER:
+                float winterWeather = Random.Range(0f, 1f);
+                if (winterWeather < 0.4f) {
+                    return Weather.SNOW;
+                }
+                else if (winterWeather < 0.8f) {
+                    return Weather.SUNNY;
+                }
+                else if (winterWeather < 0.9f) {
+                    return Weather.RAIN;
+                }
+                else {
+                    return Weather.NONE;
+                }
+                break;
+            default: return Weather.NONE;
+        }
     }
 
     public void ChangeSeason(Season seasonType) {
@@ -66,84 +227,131 @@ public class WeatherManager : MonoBehaviour
         }
     }
 
+    private void UpdateSeason() {
+        this.seasonTime -= Time.deltaTime;
+
+        switch (currentSeason) {
+            case Season.SPRING:
+                LerpLight(defaultLightColor, defaultLightIntensity + 0.15f);
+
+                if (this.seasonTime <= 0f) {
+                    ChangeSeason(Season.SUMMER);
+                    this.seasonTime = this.summerTime;
+                }
+                break;
+            case Season.SUMMER:
+                if (this.seasonTime <= 0f) {
+                    ChangeSeason(Season.AUTUMN);
+                    this.seasonTime = this.autumnTime;
+                }
+                break;
+            case Season.AUTUMN:
+                LerpLight(autumnColor, autumnLightIntensity + 0.1f);
+
+                if (this.seasonTime <= 0f) {
+                    ChangeSeason(Season.WINTER);
+                    this.seasonTime = this.winterTime;
+                }
+                break;
+            case Season.WINTER:
+                LerpLight(winterColor, winterLightIntensity + 0.15f);
+
+                if (this.seasonTime <= 0f) {
+                    this.currentYear ++;
+                    ChangeSeason(Season.SPRING);
+                    this.seasonTime = this.springTime;
+                }
+                break;
+        }
+    }
+
+    private void LerpLight(Color c, float intensity) {
+        sunlight.color = Color.Lerp(sunlight.color, c, 0.2f * Time.deltaTime);
+        sunlight.intensity = Mathf.Lerp(sunlight.intensity, intensity, 0.2f * Time.deltaTime);
+    }
+
+    private void FixedUpdate(){
+        UpdateSeason();
+        UpdateWeather();
+    }
+
     public void ChangeWeather(Weather weatherType) {
         if (weatherType != this.currentWeather) {
             switch (weatherType) {
                 case Weather.SUNNY:
                     currentWeather = Weather.SUNNY;
                     this.rain.Stop();
+                    this.snow.Stop();
+                    // this.heavyRain.Stop();
+                    // this.fog.Stop();
+                    // this.wind.Stop();
+                    // this.storm.Stop();
                     break;
                 case Weather.HOTSUN:
                     currentWeather = Weather.HOTSUN;
                     this.rain.Stop();
+                    this.snow.Stop();
+                    // this.heavyRain.Stop();
+                    // this.fog.Stop();
+                    // this.wind.Stop();
+                    // this.storm.Stop();
                     break;
                 case Weather.RAIN:
                     currentWeather = Weather.RAIN;
                     this.rain.Play();
+                    this.snow.Stop();
+                    // this.heavyRain.Stop();
+                    // this.fog.Stop();
+                    // this.wind.Stop();
+                    // this.storm.Stop();
                     break;
                 case Weather.SNOW:
                     currentWeather = Weather.SNOW;
+                    this.snow.Play();
                     this.rain.Stop();
+                    // this.heavyRain.Stop();
+                    // this.fog.Stop();
+                    // this.wind.Stop();
+                    // this.storm.Stop();
+                    break;
+                case Weather.FOGGY:
+                    currentWeather = Weather.FOGGY;
+                    // this.fog.Play();
+                    this.rain.Stop();
+                    this.snow.Stop();
+                    // this.heavyRain.Stop();
+                    // this.wind.Stop();
+                    // this.storm.Stop();
+                    break;
+                case Weather.WINDY:
+                    currentWeather = Weather.WINDY;
+                    // this.wind.Play();
+                    this.rain.Stop();
+                    this.snow.Stop();
+                    // this.heavyRain.Stop();
+                    // this.fog.Stop();
+                    // this.storm.Stop();
+                    break;
+                case Weather.THUNDERSTORM:
+                    currentWeather = Weather.THUNDERSTORM;
+                    // this.storm.Play();
+                    this.rain.Stop();
+                    this.snow.Stop();
+                    // this.heavyRain.Stop();
+                    // this.fog.Stop();
+                    // this.wind.Stop();
+                    break;
+                default:
+                    currentWeather = Weather.NONE;
+                    this.rain.Stop();
+                    this.snow.Stop();
+                    // this.heavyRain.Stop();
+                    // this.fog.Stop();
+                    // this.wind.Stop();
+                    // this.storm.Stop();
                     break;
             }
         }
     }
-
-    private void FixedUpdate() {
-        this.seasonTime -= Time.deltaTime;
-
-        if (this.currentSeason == Season.SPRING) {
-            ChangeWeather(Weather.SUNNY);
-
-            LerpSunIntesity(this.sunlight, defaultLightIntensity);
-            LerpLightColor(this.sunlight, defaultLightColor);
-
-            if (this.seasonTime <= 0f) {
-                ChangeSeason(Season.SUMMER);
-                this.seasonTime = this.summerTime;
-            }
-        }
-        else if (this.currentSeason == Season.SUMMER) {
-            ChangeWeather(Weather.HOTSUN);
-
-            LerpSunIntesity(this.sunlight, summerLightIntensity);
-            LerpLightColor(this.sunlight, summerColor);
-
-            if (this.seasonTime <= 0f) {
-                ChangeSeason(Season.AUTUMN);
-                this.seasonTime = this.autumnTime;
-            }
-        }
-        else if (this.currentSeason == Season.AUTUMN) {
-            ChangeWeather(Weather.RAIN);
-
-            LerpSunIntesity(this.sunlight, autumnLightIntensity);
-            LerpLightColor(this.sunlight, autumnColor);
-
-            if (this.seasonTime <= 0f) {
-                ChangeSeason(Season.WINTER);
-                this.seasonTime = this.winterTime;
-            }
-        }
-        else if (this.currentSeason == Season.WINTER) {
-            ChangeWeather(Weather.SNOW);
-
-            LerpSunIntesity(this.sunlight, winterLightIntensity);
-            LerpLightColor(this.sunlight, winterColor);
-
-            if (this.seasonTime <= 0f) {
-                this.currentYear ++;
-                ChangeSeason(Season.SPRING);
-                this.seasonTime = this.springTime;
-            }
-        }
-    }
-
-    private void LerpLightColor(Light light, Color c) {
-        light.color = Color.Lerp(light.color, c, 0.2f * Time.deltaTime);
-    }
-
-    private void LerpSunIntesity(Light light, float intensity) {
-        light.intensity = Mathf.Lerp(light.intensity, intensity, 0.2f * Time.deltaTime);
-    }
 }
+
