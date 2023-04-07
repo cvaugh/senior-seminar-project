44c067b1a77f0d12eb0cc6eec063532b0defd7ce
Jaakob Alakulppi <jbalakulppi@gmail.com>
2023-04-07 19:55:46 -0400

    7 day Weather forecast added

--- Assets/Scripts/TimeOfDay.cs
+++ Assets/Scripts/TimeOfDay.cs
@@ -9,31 +9,30 @@ public class TimeOfDay : MonoBehaviour
     public float currentTimeOfDay = 0f;
     private float timeMultiplier = 1f;
     private float sunInitialIntensity;
+    private int currentDayOfWeek = 1;
+    private int previousDayOfWeek;
+    public int weeks = 0;
 
     private void Start() {
         sun = GameObject.Find("Directional Light").GetComponent<Light>();
 
-        if (sun == null){
+        if (sun == null) {
             Debug.LogError("No directional light found");
         }
         sunInitialIntensity = sun.intensity;
     }
 
-    private void UpdateSun()
-    {
+    private void UpdateSun() {
         sun.transform.localRotation = Quaternion.Euler((currentTimeOfDay * 360f) - 90, 170, 0);
 
         float intensityMultiplier = 1;
-        if (currentTimeOfDay <= 0.23f || currentTimeOfDay >= 0.75f)
-        {
+        if (currentTimeOfDay <= 0.23f || currentTimeOfDay >= 0.75f) {
             intensityMultiplier = 0;
         }
-        else if (currentTimeOfDay <= 0.25f)
-        {
+        else if (currentTimeOfDay <= 0.25f) {
             intensityMultiplier = Mathf.Clamp01((currentTimeOfDay - 0.23f) * (1 / 0.02f));
         }
-        else if (currentTimeOfDay >= 0.73f)
-        {
+        else if (currentTimeOfDay >= 0.73f) {
             intensityMultiplier = Mathf.Clamp01(1 - ((currentTimeOfDay - 0.73f) * (1 / 0.02f)));
         }
 
@@ -45,9 +44,41 @@ public class TimeOfDay : MonoBehaviour
 
         currentTimeOfDay += (Time.deltaTime / dayDurationSeconds) * timeMultiplier;
 
-        if (currentTimeOfDay >= 1)
-        {
+        if (currentTimeOfDay >= 1) {
             currentTimeOfDay = 0;
+            currentDayOfWeek ++;
+
+            if (currentDayOfWeek > 7) {
+                currentDayOfWeek = 1;
+                weeks ++;
+            }
         }
     }
+
+    public int GetCurrentDay() {
+        return currentDayOfWeek;
+    }
+
+    public int GetCurrentWeek() {
+        return weeks;
+    }
+
+    // public bool IsNewWeek() {
+    //     previousDayOfWeek = GetPreviousDay();
+    //     if (currentDayOfWeek == 1 && previousDayOfWeek == 7) {
+    //         return true;
+    //     }
+    //     else {
+    //         return false;
+    //     }
+    // }
+
+    public int GetPreviousDay() {
+        int previousDayOfWeek = currentDayOfWeek - 1;
+        if (previousDayOfWeek == 0) {
+            previousDayOfWeek = 7;
+        }
+        return previousDayOfWeek;
+    }
+    //method for isDayTime(), isAfternoon(), isNightTime()
 }
