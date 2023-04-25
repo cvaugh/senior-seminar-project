a0b6506b215e9680de16c5c16ea825c3e16073d0
yxsmeen <hartyasmeen@gmail.com>
2023-04-25 10:06:57 -0400

    New Mowing minigame fixed

--- Assets/Scripts/GrassMoveScript.cs
+++ Assets/Scripts/GrassMoveScript.cs
@@ -4,27 +4,36 @@ using UnityEngine;
 
 public class GrassMoveScript : MonoBehaviour
 {
-    public float moveSpeed = 3;
-    public float deadZone = 265;
+    public float moveSpeed;
+    public float deadZone = 0;
 
     public CountdownTimer countdownScript;
+    public GrassSpawnScript grassSpawnScript;
 
     // Start is called before the first frame update
     void Start()
     {
+        moveSpeed = 100;
         countdownScript = GameObject.FindGameObjectWithTag("Logic").GetComponent<CountdownTimer>();
+        grassSpawnScript = GameObject.FindGameObjectWithTag("Grass").GetComponent<GrassSpawnScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position = transform.position + (Vector3.left * moveSpeed) * Time.deltaTime;
+        if(countdownScript.getGameTime() >= 60)
+        {
+            moveSpeed = 400;
+            //grassSpawnScript.spawnGrass();
+            
+        }
         if(countdownScript.getGameTime() <= 30)
         {
-            moveSpeed = 5;
+            moveSpeed = 200;
         }if(countdownScript.getGameTime() <= 10)
         {
-            moveSpeed = 7;
+            moveSpeed = 400;
         }
         if(transform.position.x < deadZone)
         {
--- Assets/Scripts/GrassSpawnScript.cs
+++ Assets/Scripts/GrassSpawnScript.cs
@@ -8,17 +8,23 @@ public class GrassSpawnScript : MonoBehaviour
     public float spawnRate = 2;
     public float heightOffset = 20;
     private float timer = 0;
+    public CountdownTimer countdownScript;
 
 
     // Start is called before the first frame update
     void Start()
     {
         spawnGrass();
+        countdownScript = GameObject.FindGameObjectWithTag("Logic").GetComponent<CountdownTimer>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(countdownScript.getStartTime() == 3 || countdownScript.getStartTime() == 2 || countdownScript.getStartTime() == 1)
+        {
+            spawnGrass();
+        }
         if (timer < spawnRate)
         {
             timer += Time.deltaTime;
@@ -31,9 +37,9 @@ public class GrassSpawnScript : MonoBehaviour
 
     }
 
-    void spawnGrass()
+    public void spawnGrass()
     {
-        Instantiate(grass, new Vector3(transform.position.x, Random.Range(250, 270), 0), transform.rotation);
+        Instantiate(grass, new Vector3(transform.position.x, Random.Range(0, 1000), 0), transform.rotation);
     }
 
  
