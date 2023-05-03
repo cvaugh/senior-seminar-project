87de308e52fc05f883e287b0a9d822a54da2e554
Chase Vaughan <git@cvaugh.dev>
2023-05-03 17:17:58 -0400

    Add light switches

---
+++ Assets/Scripts/LightSwitch.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class LightSwitch : Interactable {
+    public Light lightComponent;
+    private Transform switchPivot;
+
+    void Start() {
+        switchPivot = transform.GetChild(0);
+        UpdateSwitchRotation();
+    }
+
+    private void UpdateSwitchRotation() {
+        switchPivot.localRotation = Quaternion.Euler(new Vector3(0.0f, 0.0f, lightComponent.enabled ? 45.0f : -45.0f));
+    }
+
+    public override void Interact(PlayerController player) {
+        lightComponent.enabled = !lightComponent.enabled;
+        UpdateSwitchRotation();
+    }
+}
